3e438a1e9d6eaa611916b83e2bd18eb940412bc4
Pedro Castro <pedro.castro@11383-MB.local>
2024-05-12 19:44:09 -0300

    clock

--- js/clock.js
+++ js/clock.js
@@ -1,22 +1,21 @@
 function setRotation(element, rotationRatio) {
-    element.style.transform = `rotate(${rotationRatio * 360}deg)`;
-  }
+  element.style.transform = `rotate(${rotationRatio * 360 + 90}deg)`;
+}
+
+function setClock() {
+  const currentDate = new Date();
+  const secondsRatio = currentDate.getSeconds() / 60;
+  const minutesRatio = (secondsRatio + currentDate.getMinutes()) / 60;
+  const hoursRatio = (minutesRatio + currentDate.getHours() % 12) / 12;
   
-  function setClock() {
-    const currentDate = new Date();
-    const secondsRatio = currentDate.getSeconds() / 60;
-    const minutesRatio = (secondsRatio + currentDate.getMinutes()) / 60;
-    const hoursRatio = (minutesRatio + currentDate.getHours()) / 12;
-    
-    const hourHand = document.getElementById('hour-hand');
-    const minuteHand = document.getElementById('minute-hand');
-    const secondHand = document.getElementById('second-hand');
-    
-    setRotation(secondHand, secondsRatio);
-    setRotation(minuteHand, minutesRatio);
-    setRotation(hourHand, hoursRatio);
-  }
+  const hourHand = document.getElementById('hour-hand');
+  const minuteHand = document.getElementById('minute-hand');
+  const secondHand = document.getElementById('second-hand');
   
-  setInterval(setClock, 1000);
-  setClock();  // Initialize the clock
-  
+  setRotation(secondHand, secondsRatio);
+  setRotation(minuteHand, minutesRatio);
+  setRotation(hourHand, hoursRatio);
+}
+
+setInterval(setClock, 1000);
+setClock();  // Initialize the clock
